a8d9881e1ac51a7f94fb72d49c29ca0f015a00ed
Ayan9074 <Ayan9074@yahoo.com>
2021-02-01 07:56:09 +0400

    secret feature

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/templates/index.html');
 });
 
 io.on('connection', (socket) => {
